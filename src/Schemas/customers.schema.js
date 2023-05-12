3128c45bc2f98f9314475771e0528cd636d1fde5
Ivan <ivanaires32@gmail.com>
2023-05-12 16:15:23 -0300

    fix: add games

--- src/Schemas/customers.schema.js
+++ src/Schemas/customers.schema.js
@@ -1,7 +1,7 @@
 import joi from "joi"
 export const customersSchema = joi.object({
     name: joi.string().required(),
-    phone: joi.number().min(10).max(11).required(),
-    cpf: joi.number().min(11).max(11).required(),
+    phone: joi.string().min(10).max(11).required(),
+    cpf: joi.string().min(11).max(11).required(),
     birthday: joi.date()
 })
